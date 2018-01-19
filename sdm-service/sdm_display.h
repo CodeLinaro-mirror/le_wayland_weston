8fc9de9fde5adee003b5d1fbd57f2aeba67ed1ba
Srikanth Rajagopalan <rasrik@codeaurora.org>
2018-01-18 18:24:16 -0800

    sdm-service: Replace vblank with page flip events
    vblank event replacedwith page flip event to overcome
    tearing issues.
    
    Change-Id: Ibf7cb99d7fcabdcf090ec26037ffb781813cf9f6

--- sdm-service/sdm_display.h
+++ sdm-service/sdm_display.h
@@ -81,6 +81,7 @@ class SdmDisplayInterface {
     virtual SdmDisplayIntfType GetDisplayIntfType() = 0;
 
     static int GetDrmMasterFd();
+    pageflip_cb_t pageflip_cb_ = NULL;
 };
 
 class SdmNullDisplay : public SdmDisplayInterface {
@@ -207,7 +208,6 @@ class SdmDisplay : public SdmDisplayInterface, DisplayEventHandler, SdmDisplayDe
     int disable_hdr_handling_ = 0;
     bool hdr_supported_ = false;
     uint32_t hdcp_version_ = 0;
-    pageflip_cb_t pageflip_cb_ = NULL;
 };
 
 class SdmDisplayProxy {
