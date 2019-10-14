7829f2851adced0df1e31de92302050841ff2ad4
Lei Ma <leima@codeaurora.org>
2019-10-14 15:56:39 +0800

    gl-renderer: clear clip region of the view
    When this view switch from gpu to overlay, the
    view's clip region will not be recompute by overlay logic.
    So reset this region in clear_view().
    
    Change-Id: I50da541cc5e41f4bffd95fa1b5b7eb13123bd947
    Signed-off-by: Lei Ma <leima@codeaurora.org>

--- libweston/gl-renderer.c
+++ libweston/gl-renderer.c
@@ -977,6 +977,7 @@ clear_view(struct weston_view *ev, struct weston_output *output,
 
 	pixman_region32_init(&repaint);
 	pixman_region32_intersect(&repaint, &ev->transform.boundingbox, damage);
+	pixman_region32_init_rect(&ev->clip, 0, 0, 0, 0);
 	pixman_region32_subtract(&repaint, &repaint, &ev->clip);
 
 	if (!pixman_region32_not_empty(&repaint))
