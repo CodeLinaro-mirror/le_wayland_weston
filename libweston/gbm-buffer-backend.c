9c83d3a0f41812d68e8dd1d2f29955d6f4f4f721
jinjlei <jinjlei@codeaurora.org>
2021-02-18 10:47:12 +0800

    libweston: Tune the logic for gbm_buffer_destroy
    libgbm has been tuned in 3526367, gbm_bo_destroy will just close buffer fd
    created by the current process.
    The shared buffer fd need to be closed by weston itself
    
    Change-Id: I30609f4244160009253bf0dc9c869b3fa0e9165e
    Signed-off-by: jinjlei <jinjlei@codeaurora.org>

--- libweston/gbm-buffer-backend.c
+++ libweston/gbm-buffer-backend.c
@@ -1,5 +1,5 @@
 /*
-*    Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+*    Copyright (c) 2017-2019,2021, The Linux Foundation. All rights reserved.
 *
 *    Redistribution and use in source and binary forms, with or without
 *    modification, are permitted provided that the following conditions are
@@ -96,6 +96,10 @@ gbm_buffer_destroy(struct gbm_buffer *buffer)
 	gbm_bo_destroy(buffer->bo);
 	buffer->bo = NULL;
   }
+
+  // Close shared fd
+  close(buffer->fd);
+  close(buffer->metadata_fd);
 
   free(buffer);
 }
