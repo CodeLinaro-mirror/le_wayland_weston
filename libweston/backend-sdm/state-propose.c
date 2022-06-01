d913df5630bae5d8dfdd4aff2b24ba1e081b2bd8
Chirag Khurana <quic_ckhurana@quicinc.com>
2022-06-01 13:31:06 +0530

    weston: add support for DP display
    Change-Id: Idaadf8e642e212c026c230eb530fdebf3e357ef2

--- libweston/backend-sdm/state-propose.c
+++ libweston/backend-sdm/state-propose.c
@@ -28,6 +28,42 @@
  * SOFTWARE.
  */
 
+/*
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "config.h"
 
 #include <xf86drm.h>
@@ -41,10 +77,6 @@
 
 #include "linux-dmabuf.h"
 #include "presentation-time-server-protocol.h"
-
-#ifndef MULTI_DISPLAY
-extern int display_id;
-#endif
 
 void
 destroy_sdm_layer(struct sdm_layer *layer)
@@ -146,11 +178,7 @@ drm_assign_planes(struct weston_output *output_base, void *repaint_data)
 
 	output->view_count++;
 
-#ifndef MULTI_DISPLAY
-	int ret = Prepare(display_id, output);
-#else
 	int ret = Prepare(output->display_id, output);
-#endif
 	if (ret != 0) {
 		weston_log("%s: Assigning planes failed\n", __func__);
 		return;
