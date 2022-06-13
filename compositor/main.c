675fb65f36ddb9e87304d262de188a1067a48551
Ke Du <quic_kedu@quicinc.com>
2022-06-13 14:19:29 +0530

    weston: add clearBSD license
    Add clearBSD license.
    
    Change-Id: I47338fbb6f5f9277ae082546810f5e8008493ba4

--- compositor/main.c
+++ compositor/main.c
@@ -1,7 +1,6 @@
  /*
  *
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met:
@@ -51,6 +50,10 @@
  * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #include "config.h"
