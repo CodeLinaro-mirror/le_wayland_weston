7da40e967f06b1008a673068bc08468174d0d943
zijizhou <quic_zijizhou@quicinc.com>
2022-11-01 14:24:18 +0800

    fix no-display from drm-backend after hotplug
    weston: calling drm_head_assign_connector_info to
    retrieve connector modes
    
    Change-Id: Iec1ed62117a149db72ded8b43ba092178264ef16

--- libweston/backend-drm/drm.c
+++ libweston/backend-drm/drm.c
@@ -2022,6 +2022,7 @@ drm_head_update_info(struct drm_head *head)
 {
 	drmModeConnector *connector;
 
+	weston_log("%s: updating info\n", __FUNCTION__);
 	connector = drmModeGetConnector(head->backend->drm.fd,
 					head->connector_id);
 	if (!connector) {
@@ -2030,10 +2031,10 @@ drm_head_update_info(struct drm_head *head)
 		return;
 	}
 
-#ifndef BUILD_DRM_COMPOSITOR
+//#ifndef BUILD_DRM_COMPOSITOR
 	if (drm_head_assign_connector_info(head, connector) < 0)
 		drmModeFreeConnector(connector);
-#endif
+//#endif
 
 	if (head->base.device_changed)
 		drm_head_log_info(head, "updated");
@@ -2223,6 +2224,7 @@ drm_backend_update_heads(struct drm_backend *b, struct udev_device *drm_device)
 
 		head = drm_head_find_by_connector(b, connector_id);
 		if (head) {
+			weston_log("head is present for connector:%d \n", connector_id);
 			drm_head_update_info(head);
 		} else {
 			head = drm_head_create(b, connector_id, drm_device);
@@ -2363,6 +2365,7 @@ udev_drm_event(int fd, uint32_t mask, void *data)
 	event = udev_monitor_receive_device(b->udev_monitor);
 
 	if (udev_event_is_hotplug(b, event)) {
+		weston_log("%s: HOTPLUG event\n", __FUNCTION__);
 		if (udev_event_is_conn_prop_change(b, event, &conn_id, &prop_id))
 			drm_backend_update_conn_props(b, conn_id, prop_id);
 		else
