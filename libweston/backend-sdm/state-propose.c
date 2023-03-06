1c92dfdc4d77fcfee10206728269ea8d7dff4b47
Chirag Khurana <quic_ckhurana@quicinc.com>
2023-03-06 18:22:45 +0530

    weston: pass buffer object for skipped layers
    Change-Id: I63464b8104ae8b567a7bba2c06ab4709ec026458

--- libweston/backend-sdm/state-propose.c
+++ libweston/backend-sdm/state-propose.c
@@ -101,8 +101,7 @@ create_sdm_layer(struct drm_output *output, struct weston_view *ev,
 	layer->acquire_fence_fd = ev->surface->acquire_fence_fd;
 	layer->is_cursor = is_cursor;
 	layer->is_skip = is_skip;
-	if (!is_skip)
-		layer->fb = drm_fb_get_from_view(output, ev);
+	layer->fb = drm_fb_get_from_view(output, ev);
 
 	pixman_region32_init(&layer->overlap);
 	pixman_region32_copy(&layer->overlap, overlap);
