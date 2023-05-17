cbee06e8d49423191fbb14214a99f3b018a1fb68
Chirag Khurana <quic_ckhurana@quicinc.com>
2023-05-17 17:19:27 +0530

    weston: add P010 format support
    Change-Id: Ie83f3d616c4ae542053c3b2be052203631d7cf6a

--- libweston/backend-sdm/sdm-service/sdm_display.cpp
+++ libweston/backend-sdm/sdm-service/sdm_display.cpp
@@ -1436,6 +1436,9 @@ LayerBufferFormat SdmDisplay::GetSDMFormat(uint32_t src_fmt, struct LayerGeometr
         case SDM_BUFFER_FORMAT_P010:
             format = sdm::kFormatYCbCr420P010;
             break;
+        case SDM_BUFFER_FORMAT_P010_VENUS:
+            format = sdm::kFormatYCbCr420P010Venus;
+            break;
         default:
             DLOGE("Unsupported format %d\n", src_fmt);
             return sdm::kFormatInvalid;
@@ -1472,6 +1475,7 @@ bool SdmDisplay::GetVideoPresenceByFormatFromGbm(uint32_t fmt)
        case GBM_FORMAT_NV12:
        case GBM_FORMAT_YCbCr_420_TP10_UBWC:
        case GBM_FORMAT_YCbCr_420_P010_UBWC:
+       case GBM_FORMAT_YCbCr_420_P010_VENUS:
        case GBM_FORMAT_P010:
             is_video_present = true;
             break;
@@ -1538,6 +1542,9 @@ uint32_t SdmDisplay::GetMappedFormatFromGbm(uint32_t fmt)
         break;
     case GBM_FORMAT_P010:
         ret = SDM_BUFFER_FORMAT_P010;
+        break;
+    case GBM_FORMAT_YCbCr_420_P010_VENUS:
+        ret = SDM_BUFFER_FORMAT_P010_VENUS;
         break;
     default:
          DLOGE("Unsupported GBM format %s\n", FourccToString(fmt));
