d412832f8cc9ab57b43f738e69b23000bd745684
Xiaoyu Huang <xiahua@qti.qualcomm.com>
2025-07-31 18:44:50 +0800

    gl-renderer: disable partial update
    Since partial update can't support semi-transparent/transparent
    background situation (especially under ivi-shell), disable this function
    and glclear framebuffer for every frame to avoid unexpected garbage.
    
    Change-Id: Ib27535825e2bbe944bc05ea7a4f83cacaf5ac30d
    Signed-off-by: Xiaoyu Huang <xiahua@qti.qualcomm.com>

--- libweston/renderer-gl/egl-glue.c
+++ libweston/renderer-gl/egl-glue.c
@@ -668,10 +668,11 @@ gl_renderer_setup_egl_extensions(struct weston_compositor *ec)
 	if (weston_check_egl_extension(extensions, "EGL_EXT_buffer_age"))
 		gr->has_egl_buffer_age = true;
 
-	if (weston_check_egl_extension(extensions, "EGL_KHR_partial_update")) {
-		assert(gr->set_damage_region);
-		gr->has_egl_partial_update = true;
-	}
+	/* Commented out this logic, since partial update can't work properly for transparent background */
+	// if (weston_check_egl_extension(extensions, "EGL_KHR_partial_update")) {
+	// 	assert(gr->set_damage_region);
+	// 	gr->has_egl_partial_update = true;
+	// }
 
 	for (i = 0; i < ARRAY_LENGTH(swap_damage_ext_to_entrypoint); i++) {
 		if (weston_check_egl_extension(extensions,
